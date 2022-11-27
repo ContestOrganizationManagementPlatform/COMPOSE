5d57dd6752a2b1d53105bc0a960d4dfafc879a1a
Eesha-Jain <eesha.jain801@gmail.com>
2022-11-26 17:02:44 -0800

    mini fix to test popup

--- src/routes/tests/[id]/index.svelte
+++ src/routes/tests/[id]/index.svelte
@@ -170,7 +170,7 @@
 			style="background-color: rgba(0,0,0,0.5); position: absolute; top: 0; bottom: 0;right:0;left: 0;z-index: 100;"
 		>
 			<div
-				style="width: 200px; height: max-content; z-index: 101;background-color: white;padding: 10px;position: relative;"
+				style="width: 300px; height: max-content; z-index: 101;background-color: white;padding: 10px;position: relative;"
 			>
 				<div style="position: absolute; top: 5px; right: 8px;">
 					<button
