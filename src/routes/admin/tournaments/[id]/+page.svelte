b09977af36c78c3c371fe2fcc9919931c1f23952
alonr619 <alan.ragoler@gmail.com>
2023-04-14 19:03:14 -0700

    Updated zip file download

--- src/routes/admin/tournaments/[id]/+page.svelte
+++ src/routes/admin/tournaments/[id]/+page.svelte
@@ -99,11 +99,18 @@
 			for (const x of full_problems) {
 				let s = "";
 				s +=
-					"\\documentclass{article}\n\\usepackage{Miscellaneous/TCMCExam}\n\n\\begin{filecontents*} {Problem.tex}\n";
+					"\\documentclass{article}\n\\usepackage{Miscellaneous/MustangMath}\n\n\\begin{filecontents*} {Problem.tex}\n";
 				s += "%<*Tag>" + x.front_id + "%</Tag>\n\n";
 				s += "%<*Author>" + x.full_name + "%</Author>\n\n";
 				s += "%<*Problem>" + x.problem_latex + "%</Problem>\n\n";
-				s += "%<*Answer>" + x.answer_latex + "%</Answer>\n\n";
+				let finalAnsLatex = x.answer_latex.trim(); // stripping $ from both sides of answer latex because Yuuki said so
+				if (finalAnsLatex.substring(0, 1) === "$") {
+					finalAnsLatex = finalAnsLatex.substring(1);
+				}
+				if (finalAnsLatex.substring(finalAnsLatex.length - 1) === "$") {
+					finalAnsLatex = finalAnsLatex.substring(0, finalAnsLatex.length - 1);
+				}
+				s += "%<*Answer>" + finalAnsLatex + "%</Answer>\n\n";
 				s += "%<*Solution>" + x.solution_latex + "%</Solution>\n";
 				s += "\\end{filecontents*}\n\n\\begin{document}\n";
 				s += "    \\large\n";
@@ -112,7 +119,7 @@
 				s +=
 					"    \\textit{Written by:} \\textit{\\ExecuteMetaData[Problem.tex]{Author}}\\\\[0.5cm]\n";
 				s +=
-					"    \\textbf{Answer: } $\\boxed{ExecuteMetaData[Problem.tex]{Answer}}$\\\\\\\\ \n";
+					"    \\textbf{Answer: } $\\boxed{\\ExecuteMetaData[Problem.tex]{Answer}}$\\\\\\\\ \n";
 				s += "    \\ExecuteMetaData[Problem.tex]{Solution}\n";
 				s += "\\end{document}";
 				problemFolder.file(x.front_id + ".tex", s);
@@ -151,17 +158,17 @@
 				problemList += "}";
 				testFolder.file("ProblemList.tex", problemList);
 				let problemTex =
-					"\\documentclass{article}\n\\usepackage{Miscellaneous/TCMCExam}\n\n\\begin{document}\n\\input{Tests/" +
+					"\\documentclass{article}\n\\usepackage{Miscellaneous/MustangMath}\n\n\\begin{document}\n\\input{Tests/" +
 					test.test_name +
 					"/ProblemList.tex}\n\\large\n\\begin{enumerate}\n    \\foreach \\p [count=\\i] in \\List{\n        \\item \\ProblemBlurb{\\p}\n    }\n\\end{enumerate}\n\\newpage\n\\end{document}";
 				testFolder.file("Problems.tex", problemTex);
 				let solutionTex =
-					"\\documentclass{article}\n\\usepackage{Miscellaneous/TCMCExam}\n\n\\begin{document}\n\\input{Tests/" +
+					"\\documentclass{article}\n\\usepackage{Miscellaneous/MustangMath}\n\n\\begin{document}\n\\input{Tests/" +
 					test.test_name +
 					"/ProblemList.tex}\n\\large\n\\begin{enumerate}\n    \\foreach \\p [count=\\i] in \\List{\n        \\item \\SolutionBlurb{\\p}\n    }\n\\end{enumerate}\n\\newpage\n\\end{document}\n";
 				testFolder.file("Solutions.tex", solutionTex);
 				let answerTex =
-					"\\documentclass{article}\n\\usepackage{Miscellaneous/TCMCExam}\n\n\\begin{document}\n\\input{Tests/" +
+					"\\documentclass{article}\n\\usepackage{Miscellaneous/MustangMath}\n\n\\begin{document}\n\\input{Tests/" +
 					test.test_name +
 					"ProblemList.tex}\n\\large\n\\begin{enumerate}\n    \\foreach \\p [count=\\i] in \\List{\n        \\item \\UnboxedAnswer{\\p}\n    }\n\\end{enumerate}\n\\newpage\n\\end{document}\n";
 				testFolder.file("Answers.tex", answerTex);
@@ -170,10 +177,25 @@
 
 		let miscellaneousFolder = zip.folder("Miscellaneous");
 		let tcm =
-			"\\ProvidesPackage{TCMCExam}\n\\usepackage[utf8]{inputenc}\n\\usepackage{geometry} {}\n\\usepackage{amsmath,currfile,filecontents,graphicx,enumitem,catchfilebetweentags,fancyhdr,gensymb}\n\\usepackage{mathtools}\n\\usepackage{etoolbox}\n\\usepackage[export]{adjustbox}\n\\usepackage{pgffor}\n\\usepackage{tabularx}\n\\usepackage{ifthen}\n\n\\usepackage{tikz}\n\\usetikzlibrary{math}\n\n\\geometry{\n left = 1in,\n right = 1in,\n top = 0.8in,\n bottom = 0.8in\n }\n\n\\newenvironment*{dummyenv}{}{}\n\n%\\usepackage[dvipsnames]{xcolor}\n\\definecolor{mygray}{gray}{0.9}\n\n\n\\setlength{\\parindent}{0pt}\n\\setlength{\\parskip}{12pt}\n\\usepackage{enumitem}\n\\setenumerate{parsep=10pt}\n\\usepackage{etoolbox}\n\\makeatletter\n\\patchcmd{\\CatchFBT@Fin@l}{\\endlinechar\\m@ne}{}\n  {}{\\typeout{Unsuccessful patch!}}\n\\makeatother\n\n\\pagestyle{fancy}\n\\fancyhf{}\n\\chead{" +
+			"\\ProvidesPackage{MustangMath}\n\\usepackage[utf8]{inputenc}\n\\usepackage{geometry} {}\n\\usepackage{amsmath,currfile,filecontents,graphicx,enumitem,catchfilebetweentags,fancyhdr,gensymb}\n\\usepackage{mathtools}\n\\usepackage{etoolbox}\n\\usepackage[export]{adjustbox}\n\\usepackage{pgffor}\n\\usepackage{tabularx}\n\\usepackage{ifthen}\n\n\\usepackage{tikz}\n\\usetikzlibrary{math}\n\n\\geometry{\n left = 1in,\n right = 1in,\n top = 0.8in,\n bottom = 0.8in\n }\n\n\\newenvironment*{dummyenv}{}{}\n\n%\\usepackage[dvipsnames]{xcolor}\n\\definecolor{mygray}{gray}{0.9}\n\n\n\\setlength{\\parindent}{0pt}\n\\setlength{\\parskip}{12pt}\n\\usepackage{enumitem}\n\\setenumerate{parsep=10pt}\n\\usepackage{etoolbox}\n\\makeatletter\n\\patchcmd{\\CatchFBT@Fin@l}{\\endlinechar\\m@ne}{}\n  {}{\\typeout{Unsuccessful patch!}}\n\\makeatother\n\n\\pagestyle{fancy}\n\\fancyhf{}\n\\chead{" +
 			tournament.tournament_name +
 			"}\n\\cfoot{\\copyright\\ 2023 Mustang Math}\n\n\\newcommand{\\Tag}[1]{\\textbf{\\ExecuteMetaData[Problems/#1.tex]{Tag}}}\n\\newcommand{\\Problem}[1]{\\ExecuteMetaData[Problems/#1.tex]{Problem}}\n\n\\newcommand{\\Author}[1]{\\textit{Written by: \\ExecuteMetaData[Problems/#1.tex]{Author}}}\n\\newcommand{\\Answer}[1]{$\\boxed{\\ExecuteMetaData[Problems/#1.tex]{Answer}}$}\n\\newcommand{\\UnboxedAnswer}[1]{$\\ExecuteMetaData[Problems/#1.tex]{Answer}$}\n\\newcommand{\\AnswerBlurb}[1]{$\\ExecuteMetaData[Problems/#1.tex]{Answer}$}\n\n\\newcommand{\\Solution}[1]{\\ExecuteMetaData[Problems/#1.tex]{Solution}}\n\n\\newcommand{\\ProblemBlurb}[1]{\\Tag{#1}\\Problem{#1}}\n%\\newcommand{\\ProblemBlurb}[1]{\\Problem{#1}}\n\\newcommand{\\SolutionBlurb}[1]{ \\Tag{#1}\\Problem{#1}\\\\\\\\ \\Author{#1}\\\\[0.5cm] \\textbf{Answer: } \\Answer{#1}\\\\\\\\ \\Solution{#1} }";
-		miscellaneousFolder.file("TCMCExam.sty", tcm);
+		const macros = {
+			"\\ans": "\\boxed{#1}",
+			"\\Abs": "\\left\\lVert #1 \\right\\rVert",
+			"\\ang": "\\left \\langle #1 \\right \\rangle",
+			"\\set": "\\left\\{#1\\right\\}",
+			"\\paren": "\\left(#1\\right)",
+			"\\floor": "\\left\\lfloor #1 \\right\\rfloor",
+			"\\ceil": "\\left\\lceil #1 \\right\\rceil",
+			"\\VEC": "\\overrightarrow{#1}",
+			"\\Mod": "\\enspace(\\text{mod}\\ #1)",
+			"\\image": "\\includegraphics{#1}",
+		};
+		for (const x of Object.keys(macros)) {
+			tcm += "\\newcommand{" + x + "}[1]{" + macros[x] + "}";
+		}
+		miscellaneousFolder.file("MustangMath.sty", tcm);
 
 		let image_paths = await getBucketPaths("");
 		for (const x of image_paths) {
