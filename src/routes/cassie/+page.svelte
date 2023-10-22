12938270174d734eeb3e3eb6a3d5a7ac39942658
tyranasaurus <90056513+tyranasaurus@users.noreply.github.com>
2023-10-22 00:22:47 -0700

    Update +page.svelte

--- src/routes/cassie/+page.svelte
+++ src/routes/cassie/+page.svelte
@@ -3,8 +3,14 @@
 	import { get } from "svelte/store";
 	import { supabase } from "$lib/supabaseClient";
 	import ProblemList from "$lib/components/ProblemList.svelte";
+    import { getUser } from "$lib/supabase";
 	//import { supabase } from "$lib/src/supabaseClient";
 	//https://sdk.vercel.ai/docs/guides/frameworks/sveltekit
+    (async() => {
+        const user = await getUser();
+        console.log(user)
+    }());
+    
 	const promptParts = [
 		"COMPOSE - the Collaborative Online Math Problem Organization and Sharing Environment - is a storage platform for contest math problems.",
 		"Math contest organizers must query the COMPOSE database when creating math competitions.",
@@ -12,7 +18,9 @@
 		"Your job is to answer user's questions regarding the COMPOSE database to the best of your knowledge.",
 		"Each entry in the database corresponds to one math problem.",
 		"The database you have access to is a view called full_problems. Each row has the following attributes: {answer_latex: string | null, archived: boolean | null, author_id: string | null, comment_latex: string | null, created_at: string | null, difficulty: number | null, edited_at: string | null, front_id: string | null, full_name: string | null, id: number | null, nickname: string | null, problem_latex: string | null, problem_tests: string | null, solution_latex: string | null, sub_topics: string | null, topics: string | null, topics_short: string | null, unresolved_count: number | null}",
-		"Database queries should fill in the [TODO] in the following supabase-js function template: ```javascript await supabase.from('full_problems').select('*').[TODO]```",
+		"Database queries should fill in the [TODO] in the following supabase-js function template with a filter function: ```javascript await supabase.from('full_problems').select('*').[TODO]```",
+		//"Use .ilike instead of .contains for all such queries",
+		"If your message includes a database query, do not include any additional text.",
 	];
 
 	const { input, handleSubmit, messages } = useChat({
@@ -41,48 +49,32 @@
 			const regex = /```javascript(.*?)```/s;
 			const match = curMessage.content.match(regex);
 			console.log(match);
-
+			let codeBlock = curMessage.content;
 			if (match) {
-				let codeBlock = match[1].trim();
-				console.log("CODE", codeBlock);
-				//codeBlock = codeBlock.replaceAll(/(\r\n|\n|\r)/gm, "");
-				//console.log("CODE", codeBlock);
-				console.log("Created");
-				/*
-				(async () => {
-					const { data } = await supabase
-						.from("full_problems")
-						.select("*")
-						.gt("difficulty", 3)
-						.order("difficulty", { ascending: false });
-					console.log(data);
-				})();
-                */
-				const asyncFunction = new Function(
-					"supabase",
-					`
+				codeBlock = match[1].trim();
+			}
+			const asyncFunction = new Function(
+				"supabase",
+				`
                     return (async () => {
                         const { data } = ${codeBlock}
                         return data;
                     })();
                     `
-				);
-				try {
-					const result = asyncFunction(supabase)
-						.then((result) => {
-							console.log(result);
-							problems = result;
-							console.log("Async code execution completed.");
-						})
-						.catch((error) => {
-							console.error("Async code execution error:", error);
-						});
-					console.log("Code executed successfully. Result:", result);
-				} catch (error) {
-					console.error("Error executing code:", error);
-				}
-			} else {
-				console.log("No code block found.");
+			);
+			try {
+				const result = asyncFunction(supabase)
+					.then((result) => {
+						console.log(result);
+						problems = result;
+						console.log("Async code execution completed.");
+					})
+					.catch((error) => {
+						console.error("Async code execution error:", error);
+					});
+				console.log("Code executed successfully. Result:", result);
+			} catch (error) {
+				console.error("Error executing code:", error);
 			}
 		} else {
 			console.log("Not a function");
