8f634d736051fb1dc1844b84e0e999177b43e2f3
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2023-12-24 13:20:24 -0800

    animted swiping

--- src/routes/grading/+page.svelte
+++ src/routes/grading/+page.svelte
@@ -1,39 +1,54 @@
 <script lang="ts">
-    let test = "MMT 2024";
-    let round = "Team Round";
-    let numberOfProblems = 10;
+	let test = "MMT 2024";
+	let round = "Team Round";
+
+	let names = [  //0 = Not started, 1 = In Progress, 2 = Done
+		{ prob_id: 1, status: 2 },
+		{ prob_id: 2, status: 1 },
+		{ prob_id: 3, status: 0 },
+		{ prob_id: 4, status: 0 },
+	];
 </script>
 
 <div>
-    <h1>Grade {test}</h1>
-    <p>Round: {round}</p>
+	<h1>Grade {test}</h1>
+	<p>Round: {round}</p>
 
-    <br />
-    
-    <p style="font-style: italic;">Pick a problem to grade</p>
+	<br />
 
-    {#each {length: numberOfProblems} as _, i}
-        <div>
-            <a class="problemContainer" href="/grading/{i+1}">Problem {i + 1}</a>
-        </div>
-    {/each}
+	<p style="font-style: italic;">Pick a problem to grade</p>
+
+	{#each names as item, index (item.prob_id)}
+		<div>
+			<a class="problemContainer" href="/grading/{item.prob_id}">
+				Problem {item.prob_id}:
+				{#if item.status == 0}
+					<span style="color: var(--incorrect-text)">Not Started</span>
+				{:else if item.status == 1}
+					<span style="color: var(--unsure-text)">In Progress</span>
+				{:else}
+					<span style="color: var(--correct-text)">Done</span>
+				{/if}
+			</a>
+		</div>
+	{/each}
 </div>
 
 <style>
-    .problemContainer {
-        background-color: white;
-        border: 3px solid var(--primary-tint);
-        padding: 10px;
-        margin: 10px;
-        border-radius: 20px;
-        text-align: center;
-        font-weight: bold;
-        text-decoration: none;
-        display: block;
-        color: var(--text-color-dark);
-    }
+	.problemContainer {
+		background-color: white;
+		border: 3px solid var(--primary-tint);
+		padding: 10px;
+		margin: 10px;
+		border-radius: 20px;
+		text-align: center;
+		font-weight: bold;
+		text-decoration: none;
+		display: block;
+		color: var(--text-color-dark);
+	}
 
-    .problemContainer:hover {
-        background-color: var(--primary-tint);
-    }
+	.problemContainer:hover {
+		background-color: var(--primary-tint);
+	}
 </style>
--- src/routes/grading/[problem]/+page.svelte
+++ src/routes/grading/[problem]/+page.svelte
@@ -1,112 +1,122 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-	import Button from '$lib/components/Button.svelte';
+	import { page } from "$app/stores";
+    import { onMount } from 'svelte';
     import { swipeable } from '@react2svelte/swipeable';
+    import Button from '$lib/components/Button.svelte';
 
-	let test = 'MMT 2024';
-	let round = 'Team Round';
-	let answer = 1024;
-	let imageSrc = '/gradingImage.png';
+    let test = 'MMT 2024';
+    let round = 'Team Round';
+    let answer = 1024;
 
-	function onCorrect() {
-		alert('Correct!');
-	}
+    // Track the current card index
+    let currentCardIndex = 0;
+    let cards = [
+        { image: '/gradingImage.png' },
+        { image: '/logo.png' },
+        { image: '/gradingImage.png' }
+    ];
 
-	function onIncorrect() {
-		alert('Incorrect!');
-	}
-
-	function onUnsure() {
-		alert('Unsure');
-	}
-
-	function onGoBack() {
-		alert('Go back');
-	}
-
-	function handleKeyPress(event) {
-		if (event.key === 'x') onIncorrect();
-		else if (event.key === 'c') onUnsure();
-		else if (event.key === 'v') onCorrect();
-	}
-
-    function handler(e) {
-        if (e.detail.dir == "Left") onCorrect();
-        else if (e.detail.dir == "Right") onIncorrect();
-        else if (e.detail.dir == "Down") onUnsure();
+    function handleGoBack() {
+        alert('Return prev ans');
     }
+
+    // Handle swipe actions
+    function handleSwipe(direction) {
+        if (direction == "Left") { // Correct
+            alert('Correct!');
+        } else if (direction == "Right") { // Incorrect
+            alert('Incorrect!');
+        } else if (direction == "Down") { // Unsure
+            alert('Unsure!');
+        }
+
+        // Move to the next card
+        currentCardIndex++;
+    }
+
+    onMount(() => {
+        // Load initial card data
+    });
 </script>
 
-<svelte:window on:keydown|preventDefault={handleKeyPress} />
-
-<div use:swipeable on:swiped={handler}>
-	<h1>Grade {test}</h1>
-	<div class="flex">
-		<div class="sideBySide">
-			<p>{round}</p>
-			<p style="margin-left: 20px">Problem #{$page.params.problem}</p>
-		</div>
-	</div>
-	<br />
-	<h2>{answer}</h2>
-	<br />
-	<Button title="Go Back" action={onGoBack} />
-	<br /><br />
-	<div class="picture">
-		<img src={imageSrc} alt="Grading" />
-	</div>
-	<br />
-	<div class="flex">
-		<button
-			style="background-color: #FF9999; color: #AD2828;"
-			on:click={onIncorrect}>X</button
-		>
-		<button
-			style="background-color: #FFFB99; color: #7C7215;"
-			on:click={onCorrect}>?</button
-		>
-		<button
-			style="background-color: #9BFF99; color: #157C20;"
-			on:click={onUnsure}>✔</button
-		>
-	</div>
-	<br />
+<div>
+    <h1>Grade {test}</h1>
+    <div class="flex">
+        <div class="sideBySide">
+            <p>{round}</p>
+            <p style="margin-left: 20px">Problem #{$page.params.problem}</p>
+        </div>
+    </div>
+    <br />
+    <h2>{answer}</h2>
+    <br />
+    <Button title="Go Back" href="/grading" />
+    <br /><br />
+    <!-- Swipeable card container with transition -->
+    <div class="picture" use:swipeable on:swiped={handleSwipe} style="transform: translateX(-${currentCardIndex * 100}%)">
+        {#if cards[currentCardIndex]}
+            <img src={cards[currentCardIndex].image} alt="Grading" />
+        {:else}
+            <p>No more problems</p>
+        {/if}
+    </div>
+    <br />
+    <div class="flex">
+        <button
+            style="background-color: var(--return); color: var(--return-text);"
+            on:click={() => handleGoBack()}
+        >↩</button>
+        <button
+            style="background-color: var(--incorrect); color: var(--incorrect-text);"
+            on:click={() => handleSwipe('left')}
+        >X</button>
+        <button
+            style="background-color: var(--unsure); color: var(--unsure-text);"
+            on:click={() => handleSwipe('right')}
+        >?</button>
+        <button
+            style="background-color: var(--correct); color: var(--correct-text);"
+            on:click={() => handleSwipe('down')}
+        >✔</button>
+    </div>
+    <br />
 </div>
 
 <style>
-	h1 {
-		margin-bottom: 5px;
-	}
+    h1 {
+        margin-bottom: 5px;
+    }
 
-	.sideBySide {
-		display: flex;
-	}
+    .sideBySide {
+        display: flex;
+    }
 
-	.picture {
-		background-color: var(--primary-tint);
-		max-width: 500px;
+    .picture {
+        background-color: var(--primary-tint);
+        max-width: 500px;
         width: 100%;
-		margin: auto;
-		padding: 10px;
-		border: 5px solid var(--primary-dark);
-		border-radius: 15px;
-	}
+        margin: auto;
+        padding: 10px;
+        border: 5px solid var(--primary-dark);
+        border-radius: 15px;
+        transition: transform 0.3s ease; /* Add transition for smooth movement */
+    }
 
-	img {
-		width: 100%;
-	}
+    img {
+        width: 100%;
+    }
 
-	button {
-		width: 90px;
-		height: 40px;
-		border: 2px solid black;
-		margin: 10px;
-		font-size: 22px;
-		border-radius: 10px;
-		font-weight: bold;
-	}
+    button {
+        width: 90px;
+        height: 40px;
+        border: 2px solid black;
+        margin: 10px;
+        font-size: 22px;
+        border-radius: 10px;
+        font-weight: bold;
+    }
 
-	button:hover {
-		cursor: pointer;
-	}
+    button:hover {
+        cursor: pointer;
+    }
 </style>
