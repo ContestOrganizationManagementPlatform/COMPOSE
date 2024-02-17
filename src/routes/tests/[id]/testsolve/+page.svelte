786d5e96b7927629f47b51220c877bbf495494ee
stanfordmathtournament <146799421+stanfordmathtournament@users.noreply.github.com>
2024-02-16 23:16:28 -0800

    Update +page.svelte

--- src/routes/tests/[id]/testsolve/+page.svelte
+++ src/routes/tests/[id]/testsolve/+page.svelte
@@ -10,7 +10,6 @@
 	import Loading from "$lib/components/Loading.svelte";
 	import { formatDate } from "$lib/formatDate.js";
 	import Modal from "$lib/components/Modal.svelte";
-	import { formatDate } from "$lib/formatDate.js";
 	import Launch from "carbon-icons-svelte/lib/Launch.svelte";
 	import TestsolveList from "$lib/components/TestsolveList.svelte";
 	import Button from "$lib/components/Button.svelte";
