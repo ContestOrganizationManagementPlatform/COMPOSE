7ee1a7481030b082ddf80f57bac9a614967b4f52
tyranasaurus <90056513+tyranasaurus@users.noreply.github.com>
2024-03-02 12:31:08 -0800

    File Unpacker

--- src/lib/components/FileUpload.svelte
+++ src/lib/components/FileUpload.svelte
@@ -1,94 +1,76 @@
 <script>
 	import JSZip from "jszip";
-	import pdfjsLib from "pdfjs-dist";
-
-	export let splitPdf = false;
 
 	let files;
+	let processedFiles = [];
 
 	$: if (files) {
-		// Note that `files` is of type `FileList`, not an Array:
-		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
-		console.log(files);
-
-		for (const file of files) {
-			console.log(`${file.name}: ${file.size} bytes`);
-		}
+		(async () => {
+			await handleFileUpload();
+		})();
 	}
 
-	async function handleFileUpload(event) {
-		console.log("FILE UPLOAD");
-		console.log(event);
-		const uploadedFiles = event.target.files;
-		for (let i = 0; i < uploadedFiles.length; i++) {
-			const file = uploadedFiles[i];
-			if (file.type === "application/zip") {
+	async function handleFileUpload() {
+		console.log(files);
+		for (let i = 0; i < files.length; i++) {
+			const file = files[i];
+			console.log(file);
+			if (
+				file.type === "application/zip" ||
+				file.type === "application/x-zip-compressed"
+			) {
+				//console.log("zip found");
 				await handleZipFile(file);
-			} else if (file.type === "application/pdf" && splitPdf) {
-				await handlePdfFile(file);
+				//console.log("zip handled");
 			} else {
-				files.push(file);
-				processFile(file);
+				processedFiles.push(file);
+				// Perform your desired action on individual processedFiles
+				// For example: processFile(file);
 			}
+			console.log(i); // The value of i is not increasing because it is inside an asynchronous function
 		}
 	}
 
 	async function handleZipFile(zipFile) {
 		const zip = new JSZip();
 		const zipData = await zip.loadAsync(zipFile);
-		const extractedFiles = [];
+		const promises = [];
+
+		console.log(zipData);
 
 		zipData.forEach(async (relativePath, zipEntry) => {
-			const extractedFileData = await zipEntry.async("arraybuffer");
-			const extractedFile = new File([extractedFileData], zipEntry.name);
-			extractedFiles.push(extractedFile);
-			// Perform your desired action on each extracted file
-			// For example: processFile(extractedFile);
+			const promise = new Promise(async (resolve, reject) => {
+				const extractedFileData = await zipEntry.async("arraybuffer");
+				const extractedFile = new File([extractedFileData], zipEntry.name);
+				console.log(extractedFile);
+				resolve(extractedFile);
+			});
+			promises.push(promise);
 		});
 
-		files = [...files, ...extractedFiles];
-	}
-
-	async function handlePdfFile(pdfFile) {
-		const pdfData = await pdfjsLib.getDocument(pdfFile).promise;
-		const numPages = pdfData.numPages;
-
-		for (let i = 1; i <= numPages; i++) {
-			const page = await pdfData.getPage(i);
-			const pageData = await page.getOperatorList();
-			const singlePagePdfData = new Uint8Array(await pageData.stream.bytes);
-			const singlePagePdfFile = new File(
-				[singlePagePdfData],
-				`${pdfFile.name}_page_${i}.pdf`
-			);
-			files.push(singlePagePdfFile);
-			// Perform your desired action on each single page PDF
-			// For example: processFile(singlePagePdfFile);
-		}
+		Promise.all(promises).then((results) => {
+			console.log(results);
+			processedFiles = [...processedFiles, ...results];
+		});
 	}
 
 	function processFile(file) {
-		// Function to process each file
+		// Presumably do something here to process the files/turn into JPEGs, etc.
 		console.log("Processing file:", file.name);
 	}
 </script>
 
 <label>
 	Upload Files:
-	<input
-		type="file"
-		multiple
-		bind:files
-		on:change={(e) => {
-			console.log("ONCHANGE");
-			handleFileUpload(e);
-		}}
-	/>
+	<input type="file" multiple bind:files />
 </label>
 
-{#each files as file}
-	<div>{file.name}</div>
-{/each}
+{#if processedFiles.length > 0}
+	<h2>Selected processedFiles:</h2>
+	{#each processedFiles as file}
+		<p>{file.name} ({file.size} bytes)</p>
+	{/each}
+{/if}
 
 <style>
 	/* Your CSS styles here */
