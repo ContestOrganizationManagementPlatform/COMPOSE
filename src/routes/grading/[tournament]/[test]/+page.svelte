94cd7bccfb07ec417ce281988298df2d754fba74
tyranasaurus <90056513+tyranasaurus@users.noreply.github.com>
2024-03-08 20:01:48 -0800

    Update +page.svelte

--- src/routes/grading/[tournament]/[test]/+page.svelte
+++ src/routes/grading/[tournament]/[test]/+page.svelte
@@ -349,14 +349,7 @@
 
 <div>
 	<h1>Grade {test}</h1>
-	<div class="flex">
-		<div class="sideBySide">
-			<p>{round}</p>
-			<p style="margin-left: 20px">Problem #{currentCardIndex + 1}</p>
-		</div>
-	</div>
-	<br />
-	<h2>{answer}</h2>
+
 	<br />
 	<Button title="Go Back" href="/grading" />
 	<br /><br />
@@ -368,34 +361,44 @@
 		bind:this={card}
 	>
 		{#if gradeQueue[currentCardIndex]}
+			<div class="flex">
+				<div class="sideBySide">
+					<p>{round}</p>
+					<p style="margin-left: 20px">Problem #{currentCardIndex + 1}</p>
+				</div>
+			</div>
+			<br />
+			<h2>{gradeQueue[currentCardIndex].answer_latex}</h2>
 			<ImageZoomer
 				imageUrl={gradeQueue[currentCardIndex].image}
 				inputCoordinates={calculateDimensions(gradeQueue[currentCardIndex])}
 			/>
+			<br />
+			<div class="flex">
+				<button
+					style="background-color: #999999; color: #282828;"
+					on:click={async () => handleAction("return")}>↩ (Z)</button
+				>
+				<button
+					style="background-color: #ff9999; color: #AD2828;"
+					on:click={async () => handleAction("incorrect")}>X (X)</button
+				>
+				<button
+					style="background-color: #FFFB99; color: #7C7215;"
+					on:click={async () => handleAction("unsure")}>? (C)</button
+				>
+				<button
+					style="background-color: #9BFF99; color: #157C20;"
+					on:click={async () => handleAction("correct")}>✔ (V)</button
+				>
+			</div>
+			<br />
 		{:else}
-			<p>No more problems</p>
+			<p>No more problems - check back later!</p>
 		{/if}
+		Number of problems remaining in queue: {gradeQueue.length -
+			currentCardIndex}
 	</div>
-	<br />
-	<div class="flex">
-		<button
-			style="background-color: #999999; color: #282828;"
-			on:click={async () => handleAction("return")}>↩ (Z)</button
-		>
-		<button
-			style="background-color: #ff9999; color: #AD2828;"
-			on:click={async () => handleAction("incorrect")}>X (X)</button
-		>
-		<button
-			style="background-color: #FFFB99; color: #7C7215;"
-			on:click={async () => handleAction("unsure")}>? (C)</button
-		>
-		<button
-			style="background-color: #9BFF99; color: #157C20;"
-			on:click={async () => handleAction("correct")}>✔ (V)</button
-		>
-	</div>
-	<br />
 </div>
 
 <style>
