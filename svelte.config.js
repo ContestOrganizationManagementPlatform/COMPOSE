bbdb7044c300748a3e17a16c57d7dc3d6ceb60c0
tyranasaurus <90056513+tyranasaurus@users.noreply.github.com>
2024-03-10 06:22:18 -0700

    tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 import { optimizeImports } from "carbon-preprocess-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess(), optimizeImports()],
+	preprocess: [preprocess(), optimizeImports(), vitePreprocess({})],
 	kit: {
 		adapter: adapter(),
 	},
