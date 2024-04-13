5319472e608995cd6354119c1992181c69473fad
Francis Chua <fqchua@stanford.edu>
2024-04-13 07:46:09 -0700

    remove resolve-conflicts, add urlparam to override

--- src/routes/grading/[tournament]/[test]/+page.svelte
+++ src/routes/grading/[tournament]/[test]/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import Grading from "$lib/components/Grading.svelte";
+
+	let is_override = $page.url.searchParams.has('override');
 </script>
 
 <Grading
 	tournamentId={parseInt($page.params.tournament)}
 	testId={parseInt($page.params.test)}
+  showGrades={is_override}
+  onlyConflicted={is_override}
+  disableUnsure={is_override}
 />
